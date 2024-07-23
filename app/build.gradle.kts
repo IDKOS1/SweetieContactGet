e1865b0a6eb71f651ff048e954ee818680a73449
Orinugoori <bigbangok@naver.com>
2024-07-23 11:52:12 +0900

    Fix: minSDK, style수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.sweetcontactget"
-        minSdk = 26
+        minSdk = 31
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
