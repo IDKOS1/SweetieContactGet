a0ea782fcdf8ade567fba1a08d375c4bd51d8d47
ljm125 <147472799+ljm125@users.noreply.github.com>
2024-07-26 19:26:15 +0900

    "Feature : swipe 시 CallingDialog로 이동하도록 기능 구현"

--- app/src/main/java/com/example/sweetcontactget/AddContactActivity.kt
+++ app/src/main/java/com/example/sweetcontactget/AddContactActivity.kt
@@ -7,7 +7,6 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.sweetcontactget.data.Contact
 import com.example.sweetcontactget.data.DataObject
 import com.example.sweetcontactget.data.SweetieInfo
 import com.example.sweetcontactget.databinding.ActivityAddContactBinding
--- app/src/main/java/com/example/sweetcontactget/data/DataObject.kt
+++ app/src/main/java/com/example/sweetcontactget/data/DataObject.kt
@@ -12,6 +12,9 @@ object DataObject {
         ContactApplication.applicationContext()
     }
 
+    fun getValidKeys():List<Int>{
+        return contactMap.keys.toList()
+    }
     fun getSweetieInfo(sweetieId: Int): SweetieInfo {
         return contactMap[sweetieId]!!
     }
